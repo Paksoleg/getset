d2cdccb45616f34f00895db27eed2f558707cdf2
Oleg <>
2021-05-28 11:18:40 +0400

    Initial commit

---
+++ src/com/company/Encapsulation.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public  class Encapsulation {
+    private   int a, b;
+    public double pi = Math.PI;
+    public int showResalt() {
+        return a+b;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Encapsulation encapsulation = new Encapsulation();
+        System.out.println(encapsulation.pi);
+        encapsulation.pi = 3.14;
+    }
+}
